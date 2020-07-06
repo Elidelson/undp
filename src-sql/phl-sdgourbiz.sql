e85894c96fa4a60ddc08d703636bac5b490ed337
Dara Bunhim <dara.bunhim@undp.org>
2020-07-06 02:56:01 +0000

    Added phl-sdgourbiz.sql

---
+++ src-sql/phl-sdgourbiz.sql
@@ -0,0 +1,34 @@
+create table dbo.sdgourbiz
+(
+    Conglomorate 	VARCHAR(250), 	
+    Company 	VARCHAR(250), 	
+    Initiative 	VARCHAR(250), 	
+    Narrative 	VARCHAR(500), 	
+    Topics 	VARCHAR(250), 	
+    Aspect 	VARCHAR(1000), 	
+    Target 	VARCHAR(1000), 	
+    Source 	VARCHAR(250), 	
+    CSR_Core_Business 	VARCHAR(250), 	
+    Location 	VARCHAR(250), 	
+    province_name 	VARCHAR(250), 	
+    municity_name 	VARCHAR(250), 	
+    barangay_name 	VARCHAR(250), 	
+    province_name_res 	VARCHAR(250), 	
+    province_code_res 	VARCHAR(250), 	
+    municity_name_res 	VARCHAR(250), 	
+    municity_code_res 	VARCHAR(250), 	
+    barangay_name_res 	VARCHAR(250), 	
+    barangay_code_res 	VARCHAR(250), 	
+    score_res 	VARCHAR(250)
+)
+GO
+
+create TABLE dbo.sdg_target
+(
+    goal_number 	VARCHAR(10),
+    goal_description 	VARCHAR(5000), 	
+    target_number 	VARCHAR(10),
+    target_description 	VARCHAR(5000),
+    indicator_number 	VARCHAR(10)
+)
+GO
